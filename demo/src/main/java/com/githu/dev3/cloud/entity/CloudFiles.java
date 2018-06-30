271cfe5a441230619c51d5e62de0d3e9f10b9fd4
daniel0625 <349047855@qq.com>
2018-06-30 23:56:34 +0800

    delete unuse controller

--- demo/src/main/java/com/githu/dev3/cloud/entity/CloudFiles.java
+++ demo/src/main/java/com/githu/dev3/cloud/entity/CloudFiles.java
@@ -19,9 +19,9 @@ import lombok.Setter;
  *
  */
 @Entity
-@Table(name = "cloud_files" )
-//@Getter
-//@Setter
+@Table(name = "CloudFiles" )
+@Getter
+@Setter
 public class CloudFiles {
 
 	@Id
@@ -29,11 +29,8 @@ public class CloudFiles {
     @Column(name = "ID")
 	private Long id;
 	
-	@Column(name = "FILE_NAME", nullable = true, length = 200)
+	@Column(name = "FILE_NAME", nullable = true, length = 500)
 	private String fileName;
-	
-	@Column(name = "REAL_NAME", nullable = true, length = 200)
-	private String realName;
 	
 	@Column(name = "UPLOAD_TIME")
 	private Date uploadTime;
@@ -41,64 +38,11 @@ public class CloudFiles {
 	@Column(name = "UPLOAD_USER", nullable = true, length = 50)
 	private String uploadUser;
 
-	
-	public CloudFiles() {
-	}
-
-	public CloudFiles(Long id, String fileName, String realName, Date uploadTime, String uploadUser) {
+	public CloudFiles(Long id, String fileName, Date uploadTime, String uploadUser) {
 		super();
 		this.id = id;
 		this.fileName = fileName;
-		this.realName = realName;
 		this.uploadTime = uploadTime;
-		this.uploadUser = uploadUser;
-	}
-	
-	public CloudFiles(String fileName, String realName, Date uploadTime, String uploadUser) {
-		super();
-		this.fileName = fileName;
-		this.realName = realName;
-		this.uploadTime = uploadTime;
-		this.uploadUser = uploadUser;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public String getRealName() {
-		return realName;
-	}
-
-	public void setRealName(String realName) {
-		this.realName = realName;
-	}
-
-	public Date getUploadTime() {
-		return uploadTime;
-	}
-
-	public void setUploadTime(Date uploadTime) {
-		this.uploadTime = uploadTime;
-	}
-
-	public String getUploadUser() {
-		return uploadUser;
-	}
-
-	public void setUploadUser(String uploadUser) {
 		this.uploadUser = uploadUser;
 	}
 
--- demo/src/main/java/com/githu/dev3/demo/DemoApplication.java
+++ demo/src/main/java/com/githu/dev3/demo/DemoApplication.java
@@ -1,33 +1,18 @@
 package com.githu.dev3.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.githu.dev3.cloud.service.StorageService;
-import com.githu.dev3.cloud.util.StorageProperties;
-
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.githu.dev3.cloud"})
+@ComponentScan(basePackages = {"com.githu.dev3.cloud.controller"})
 @EnableJpaRepositories("com.githu.dev3.cloud.repository")
 @EntityScan("com.githu.dev3.cloud.entity")
-@EnableConfigurationProperties(StorageProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-//    @Bean
-//    CommandLineRunner init(StorageService storageService) {
-//        return (args) -> {
-//            storageService.deleteAll();
-//            storageService.init();
-//        };
-//    }
 }
